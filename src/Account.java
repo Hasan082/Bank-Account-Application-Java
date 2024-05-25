570fe9576dca8c3cebcee79d621219b3d006c853
Md Hasanuzzaman <dr.has82@gmail.com>
2024-05-26 02:50:35 +0600

    [ADDED] SetAccountNumber method created

--- src/Account.java
+++ src/Account.java
@@ -1,9 +1,11 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public abstract class Account {
     // List of common properties for checking and savings accounts
     String name;
     String sSn;
     double balance;
-
+    static int id = 10000;
     String accountNumber;
     double rate;
 
@@ -12,8 +14,19 @@ public abstract class Account {
         this.name = name;
         this.sSn = sSn;
         balance = initDeposit;
+
+        id++;
+        this.accountNumber = setAccountNumber();
         System.out.println("Name: " + name);
         System.out.println("SSN: " + sSn);
         System.out.println("Initial Balance: $" + balance);
+        System.out.println("Account Number: " + accountNumber);
+    }
+
+    private String setAccountNumber() {
+        String lastTwoOfSSN = sSn.substring(sSn.length() - 2, sSn.length());
+        int uniqueID = id;
+        int randomNumber = ThreadLocalRandom.current().nextInt(0, 99);
+        return lastTwoOfSSN + uniqueID + randomNumber;
     }
 }
